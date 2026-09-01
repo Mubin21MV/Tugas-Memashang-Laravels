294d593809b576442f81dd646aeddc6d424af435
Mubin21MV <ainulmubin419@gmail.com>
2026-09-01 14:19:08 +0700

    fix

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TestRoutingController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [TestRoutingController::class, 'index']);
+Route::get('/form', [TestRoutingController::class, 'form']);
+Route::post('/form/store', [TestRoutingController::class, 'store'])->name('form.store');
+Route::get('/form/table', [TestRoutingController::class, 'table']);
+Route::get('/form/table/welcome', [TestRoutingController::class, 'welcome']);
